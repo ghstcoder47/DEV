66185a0ba51a684ab6e64761a42991d76bf76301
Dawid Janas <dawid.janas@dsv-gruppe.de>
2020-02-19 16:37:34 +0100

    config geändert

--- scripte/bin/setENV_default.cmd
+++ scripte/bin/setENV_default.cmd
@@ -1,6 +1,7 @@
 @ECHO OFF
 
 set DEV_TOOLS_DIR=%~dp0../../tools
+set SOURCE_CODE_ROOT=%~dp0../../../sourceCode
 
 
 IF "%JAVA_VERSION%x" == "x" (
@@ -24,7 +25,9 @@ set MAVEN_HOME=%DEV_TOOLS_DIR%/maven/%MAVEN_DIR%-%MAVEN_VERSION%
 REM set MAVEN_OPTS=-Xms1024m -Xmx3000m -Xss2M -Dmaven.surefire.debug=-Xmx3000m
 set MAVEN_OPTS=-Xms1024M -Xmx1024M -Xss1024M
 
+set GRADLE_HOME=%DEV_TOOLS_DIR%/gradle/gradle-6.1.1
+
 set MONGO_PATH=%DEV_TOOLS_DIR%/mongodb
 
 
-set PATH=%MAVEN_HOME%/bin;%JAVA_HOME%/bin;%PATH%
+set PATH=%MAVEN_HOME%/bin;%JAVA_HOME%/bin;%GRADLE_HOME%/bin;%PATH%
--- scripte/dev_cmd.cmd
+++ scripte/dev_cmd.cmd
@@ -1,10 +1,5 @@
 @ECHO OFF
 
-
-
-
-set JAVA_VERSION="jdk-8"
-
 call bin\setENV_default
 
 start CMD /K "cd ../"
